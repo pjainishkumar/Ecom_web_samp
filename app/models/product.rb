68f8975de35c3dc577a7a279446a18b809b94a9d
Jainishkumar Pandya <pjainishkumar@academic.rrc.ca>
2024-07-25 09:37:40 -0400

    added changes to validationnn

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
   has_one_attached :image
   validates :name, :price, presence: true
-  validates :price, numericality: { greater_than: 0 }
+  validates :your_field, numericality: { greater_than_or_equal_to: 0 }
 end
